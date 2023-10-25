3b395a6b46c23435352af0c0d35076364bc328ef
nneesshh <nneesshh@163.com>
2023-10-25 10:49:28 +0800

    remove redundant

--- src/net_packet.rs
+++ src/net_packet.rs
@@ -3,16 +3,11 @@ mod buffer;
 pub use buffer::Buffer;
 
 ///
-mod conn_id;
-pub use conn_id::ConnId;
-
-///
 mod net_packet_impl;
-pub use net_packet_impl::{CmdId, NetPacket, PacketType};
+pub use net_packet_impl::{CmdId, NetPacket};
 
 ///
 mod net_packet_pool;
 pub use net_packet_pool::NetPacketGuard;
-pub use net_packet_pool::{
-    take_large_packet, take_packet, take_small_packet, SMALL_PACKET_MAX_SIZE,
-};
+pub use net_packet_pool::SMALL_PACKET_MAX_SIZE;
+pub use net_packet_pool::{take_large_packet, take_packet, take_small_packet};
